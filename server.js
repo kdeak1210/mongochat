c29d2540a971e7e53c6e966d06437774ec8f1461
kyle <kdeak1210@gmail.com>
2017-08-16 23:34:59 -0400

    fixed socket missing param

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db) {
   console.log('Mongo connected...');
 
   // Connect to socket.io
-  client.on('connection', function() {
+  client.on('connection', function(socket) {
     let chat = db.collection('chats');  // create the 'chats' collection
 
     // Create function to send status
